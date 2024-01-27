1ddb79f2202d4b7415f8c306e95494236a75e7bb
ulrica-wen <145157472+ulrica-wen@users.noreply.github.com>
2024-01-26 16:51:08 -0800

    Update Video_Extraction_Tool_v3-1.py

--- Video_Extraction_Tool_v3-1.py
+++ Video_Extraction_Tool_v3-1.py
@@ -157,6 +157,7 @@ def extractCollage(collage):
 
             # reshape the frames into a grid
             if (collage == "2x2"):
+                print("2x2")
                 grid_image = np.concatenate([np.concatenate(frame_list[i:i+2], axis=1) for i in range(0, 4, 2)], axis=0)
             elif (collage == "3x3"):
                 grid_image = np.concatenate([np.concatenate(frame_list[i:i+3], axis=1) for i in range(0, 9, 3)], axis=0)
